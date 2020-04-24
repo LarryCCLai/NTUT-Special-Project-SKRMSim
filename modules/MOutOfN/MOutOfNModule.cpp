445dc1da9d68e0166d9a83a7d5ef9247560abb22
ChunChihLai <t106360217@ntut.org.tw>
2020-04-24 21:39:33 +0800

    MOutOfModule Write function

--- modules/MOutOfN/MOutOfNModule.cpp
+++ modules/MOutOfN/MOutOfNModule.cpp
@@ -1,6 +1,7 @@
 #include"MOutOfNModule.h"
-
-
+#include<iostream>
+#define R true
+#define L false
 M_Out_Of_N_Module::M_Out_Of_N_Module() {
 	this->injection = 0;
 	this->remove = 0;
@@ -17,6 +18,9 @@ void M_Out_Of_N_Module::Initialize(Config* config) {
 		this->params = new Parameters();
 		this->params->SetParams(config);
 		this->track = new MarcoCell[this->params->N_racetrack];
+		for (int i = 0; i < params->N_racetrack; i++) {
+			this->track[i].Initialize(params);
+		}
 		this->initialized = true;
 	}
 }
@@ -27,10 +31,49 @@ M_Out_Of_N_Module::~M_Out_Of_N_Module() {
 
 uint64_t M_Out_Of_N_Module::Read(Request* request) {
 	return 0;
+
 }
 
 void M_Out_Of_N_Module::Write(Request* request) {
-
+	track[request->trackIdx].Print();
+	std::cout << "\n";
+	int startPN = (request->dataIdx + 1) * params->N_DataSegment - 1;
+	int EndPN = request->dataIdx * params->N_DataSegment;
+	uint64_t sampling = 0;
+	for (int i = 0; i < params->dataWidthSegment; i++) {
+		sampling = (sampling << 1) | 1;
+	}
+	for (int PN = startPN; PN != EndPN; PN--) {
+		for (int i = 0; i < params->dataSegmentLength; i++) {
+			//this->detect++;
+			if (this->track[request->trackIdx].Read(PN) == 1) {
+				//this->shift++;
+				this->track->Shift(R);
+			}
+			else {
+				//this->shift++;
+				this->track[request->trackIdx].Delete_SHR(PN);
+			}
+		}
+		uint64_t dataS = (sampling & request->data) >> ((startPN - PN) * params->dataWidthSegment);
+		int* MOutOfNCode = this->Encode(dataS, params->dataSegmentLength, params->N_onesDataSegment);
+		for (int i = 0; i < params->dataSegmentLength; i++) {
+			if (MOutOfNCode[i] == 1) {
+				//this->shift++;
+				this->track[request->trackIdx].Shift(L);
+			}
+			else {
+				//this->shift++;
+				this->track[request->trackIdx].Insert_SHL(PN, 0);
+			}
+		}
+		for (int j = 0; j < params->dataSegmentLength; j++) {
+			std::cout << MOutOfNCode[j];
+		}
+		std::cout << " ";
+		sampling = sampling << params->dataWidthSegment;
+		delete[]MOutOfNCode;
+	}
 }
 
 void M_Out_Of_N_Module::Access(Request* request) {
