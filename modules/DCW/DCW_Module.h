0d78cb8fa8b4262a89edbc31668b29e016b1e1f5
ChunChihLai <t106360217@ntut.org.tw>
2020-04-25 02:10:12 +0800

    DCW module h

---
+++ modules/DCW/DCW_Module.h
@@ -0,0 +1,17 @@
+#ifndef DCW_MODULE_H
+#define DCW_MODULE_H
+#include"../Module.h"
+class DCW_Module:public Module{
+public:
+	DCW_Module ();
+	~DCW_Module ();
+	void Initialize(Config* config);
+	uint64_t Read(Request* request);
+	void Write(Request* request);
+	
+
+};
+
+
+#endif // !DCW_MODULE_H
+
