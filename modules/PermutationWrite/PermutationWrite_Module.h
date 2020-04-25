863f1c01ba5a239fefb558242ee8616b843c1f4b
ChunChihLai <t106360217@ntut.org.tw>
2020-04-25 09:23:39 +0800

    PermutationWrite Module h

---
+++ modules/PermutationWrite/PermutationWrite_Module.h
@@ -0,0 +1,16 @@
+#ifndef PERMUTATIONWRITE_MODULE_H
+#define PERMUTATIONWRITE_MODULE_H
+#include"../Module.h"
+
+class PermutationWrite_Module: public Module {
+public:
+	PermutationWrite_Module ();
+	~PermutationWrite_Module ();
+	void Initialize(Config* config);
+	uint64_t Read(Request* request);
+	void Write(Request* request);
+
+};
+
+#endif // PERMUTATIONWRITE_MODULE_H
+
